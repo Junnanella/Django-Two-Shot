a81157b2ad2df2b4db04f7d75f9ad83fa1366049
Junella Caringal <junellacaringal@gmail.com>
2022-05-05 15:22:37 -0700

    Made updates to models and views. Redid migrations.

--- receipts/models.py
+++ receipts/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 # import settings to access user auth
-# from django.db import settings
+from django.conf import settings
+
+USER_MODEL = settings.AUTH_USER_MODEL
+
 
 # Create your models here.
 class Categories(models.Model):
     name = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        USER_MODEL, related_name="categories", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
@@ -13,22 +19,26 @@ class Categories(models.Model):
 
 class Accounts(models.Model):
     name = models.CharField(max_length=100)
-    number = models.PositiveIntegerField(null=False)
+    number = models.CharField(max_length=20)
+    owner = models.ForeignKey(
+        USER_MODEL, related_name="accounts", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
 
 
 class Receipts(models.Model):
-    vendor = models.CharField(max_length=100)
-    total = models.FloatField(null=False)
-    tax = models.FloatField(null=False)
-    date = models.DateField(null=False)
+    vendor = models.CharField(max_length=200)
+    total = models.DecimalField(decimal_places=3, max_digits=10)
+    tax = models.DecimalField(decimal_places=3, max_digits=10)
+    date = models.DateField()
+    purchaser = models.ForeignKey(USER_MODEL, related_name="receipts", on_delete=models.CASCADE)
     category = models.ForeignKey(
         Categories, related_name="receipts", on_delete=models.PROTECT
     )
     account = models.ForeignKey(
-        Accounts, related_name="receipts", on_delete=models.CASCADE
+        Accounts, related_name="receipts", on_delete=models.CASCADE, null=True
     )
 
     def __str__(self):
--- receipts/views.py
+++ receipts/views.py
@@ -7,6 +7,9 @@ from receipts.models import Receipts, Categories, Accounts
 # import generic views from django
 from django.views.generic import ListView, CreateView
 
+# import Login requirements
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 # List Views for Receipts, Categories, and Accounts
@@ -15,11 +18,17 @@ class ReceiptsListView(ListView):
     template_name = "receipts/receipts_list.html"
     context_object_name = "receipts"
 
+    def get_queryset(self):
+        return Receipts.objects.filter(purchaser=self.request.user)
+
 
 class CategoriesListView(ListView):
     model = Categories
     template_name = "receipts/categories_list.html"
     context_object_name = "categories"
+
+    def get_queryset(self):
+        return Categories.objects.filter(owner=self.request.user)
 
 
 class AccountsListView(ListView):
@@ -27,36 +36,42 @@ class AccountsListView(ListView):
     template_name = "receipts/accounts_list.html"
     context_object_name = "accounts"
 
+    def get_queryset(self):
+        return Accounts.objects.filter(owner=self.request.user)
+
 
 # Create Views for Receipts, Categories, and Accounts
-class ReceiptCreateView(CreateView):
+class ReceiptCreateView(LoginRequiredMixin, CreateView):
     model = Receipts
     template_name = "receipts/receipt_create.html"
     fields = ["vendor", "total", "tax", "date", "category", "account"]
 
     def form_valid(self, form):
         receipt = form.save(commit=False)
+        receipt.purchaser = self.request.user
         receipt.save()
         return redirect("receipts_list")
 
 
-class CategoriesCreateView(CreateView):
+class CategoriesCreateView(LoginRequiredMixin, CreateView):
     model = Categories
     template_name = "receipts/category_create.html"
     fields = ["name"]
 
     def form_valid(self, form):
         category = form.save(commit=False)
+        category.owner = self.request.user
         category.save()
         return redirect("categories_list")
 
 
-class AccountCreateView(CreateView):
+class AccountCreateView(LoginRequiredMixin, CreateView):
     model = Accounts
     template_name = "receipts/account_create.html"
     fields = ["name", "number"]
 
     def form_valid(self, form):
         account = form.save(commit=False)
+        account.owner = self.request.user
         account.save()
         return redirect("accounts_list")
