72b72528fafdcb0544cc41796fe25b23b8329827
Junella Caringal <junellacaringal@gmail.com>
2022-05-04 19:28:37 -0700

    Setup login functionality. Created blank htmls for logout and signup. Added more links to base.html nav bar. Added a bit of basic css.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,2 +1,13 @@
 # import path from django.urls
-# import views
+from django.urls import path
+
+# import views - login and logout are builtin views with Django
+from django.contrib.auth.views import LoginView, LogoutView
+
+# import a view for signup
+
+urlpatterns = [
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    # add path for signup
+]
